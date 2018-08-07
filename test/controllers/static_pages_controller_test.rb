2822b8206fcd0fd33136c8551cd17b2feb99df49
Manon Lan <manondoublier@yahoo.fr>
2018-08-07 03:01:49 +0200

    navbar test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,12 +7,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "navbar links to home" do
+    get root_path
+    assert_select 'nav a[href=?]', root_path
+  end
+
   # not login
   test "should get login" do
     get login_path
     assert_response :success
   end
 
+ test "navbar should link to login if not logged in" do
+    get root_path
+    assert_select 'nav a.login[href=?]', new_session_path
+end
 # login
   test "should get club" do
     user = User.create(
