15ae4d74a30be1dd39133acb0b23d284b114338c
Manon Lan <manondoublier@yahoo.fr>
2018-08-07 02:55:22 +0200

    navbar test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  # not login
+  test "should get login" do
+    get login_path
+    assert_response :success
+  end
+
+# login
+  test "should get club" do
+    user = User.create(
+      first_name: 'Imogen',
+      last_name: 'Thecat',
+      password: 'jemangedupate',
+      email: 'catcat@miaou.fr'
+    )
+    get '/users'
+    assert_response :success
+  end
+
 end
 
  
